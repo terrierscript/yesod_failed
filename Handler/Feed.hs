a60fa0268b763f4a28287f2add1dfceb2292a7bf
suisho <suisho000000@gmail.com>
2013-07-01 16:21:11 +0900

    add-handler feed GET POST

---
+++ Handler/Feed.hs
@@ -0,0 +1,9 @@
+module Handler.Feed where
+
+import Import
+
+getFeedR :: Handler Html
+getFeedR = error "Not yet implemented: getFeedR"
+
+postFeedR :: Handler Html
+postFeedR = error "Not yet implemented: postFeedR"
