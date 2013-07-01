4a6e6983679af37831c84e4d4c61e9db7efefb93
suisho <suisho000000@gmail.com>
2013-07-01 17:29:14 +0900

    フィード登録&フィード一覧確認を追加

--- Handler/Feed.hs
+++ Handler/Feed.hs
@@ -10,14 +10,30 @@ import Import
 --import Yesod.Form.Nic (YesodNic, nicHtmlField)
 -- instance YesodNic App
 addingForm :: Form Feed
+
 addingForm = renderDivs $ Feed
+  -- areq type label default_value
   <$> areq  textField "URL" Nothing
   <*> areq  textField "Name" Nothing
 
+-- feed（一覧）取得
 getFeedR :: Handler Html
 getFeedR = do
+  feeds <- runDB $ selectList [] [Desc FeedUrl]
   (feedWidget, enctype) <- generateFormPost addingForm
   defaultLayout $ do
     $(widgetFile "feed")
+
+-- feed登録
 postFeedR :: Handler Html
-postFeedR = error "Not yet implemented: postFeedR"
+postFeedR = do
+  feeds <- runDB $ selectList [] [Desc FeedUrl]
+  ((res,feedWidget),enctype) <- runFormPost addingForm
+  $(logInfo) "aa"
+  case res of
+    FormSuccess feed -> do
+      feedId <- runDB $ insert feed
+      redirect FeedR
+    _ -> defaultLayout $ do
+      setTitle "failed"
+      $(widgetFile "feed")
