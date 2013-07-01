32971b3c7df28e621dac6c6164f80097dbbb692c
suisho <suisho000000@gmail.com>
2013-07-01 15:06:46 +0900

    add loggign

--- Handler/Echo.hs
+++ Handler/Echo.hs
@@ -3,4 +3,8 @@ module Handler.Echo where
 import Import
 
 getEchoR :: String -> Handler Html
-getEchoR = error "Not yet implemented: getEchoR"
+getEchoR theText = do
+  $(logInfo) "This is a debug log message 1"
+  defaultLayout $ do
+    $(logInfo) "This is a debug log message 2"
+    [whamlet|<div>#{theText}|]
